f3d268d770e6d7588fd0804637fb6a84e0310fd3
jlinjiang <jlinjiang@gmail.com>
2024-03-17 23:39:47 +0000

    alpinejs is pro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), alpinejs()],
   site: 'https://lnjng.com',
   server: {
     port: 9000
